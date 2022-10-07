83a842149540f4656541913dae1dc6fa179012e4
KhalilMinalang <minalang.khalil@gmail.com>
2022-10-08 06:31:08 +0800

    Completed: 8. Speed Up Reducer Development Using Ngrx Schematics

---
+++ src/app/auth/auth.reducer.ts
@@ -0,0 +1,32 @@
+import { Action, createReducer, on } from "@ngrx/store";
+import { AuthActions, AuthActionTypes } from "./auth.actions";
+import { User } from "./model/user.model";
+
+export const authFeatureKey = "auth";
+
+export interface AuthState {
+  loggedIn: boolean;
+  user: User;
+}
+
+export const initialAuthState: AuthState = {
+  loggedIn: false,
+  user: undefined,
+};
+
+// export const authReducer = createReducer(initialAuthState);
+export function authReducer(
+  state = initialAuthState,
+  action: AuthActions
+): AuthState {
+  switch (action.type) {
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload.user,
+      };
+
+    default:
+      return state;
+  }
+}
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -10,38 +10,10 @@ import { environment } from "../../environments/environment";
 import { AuthActionTypes } from "../auth/auth.actions";
 import { User } from "../auth/model/user.model";
 
-type AuthState = {
-  loggedIn: boolean;
-  user: User;
-};
-
-const initialAuthState: AuthState = {
-  loggedIn: false,
-  user: undefined,
-};
-
 // Global state
-export interface AppState {
-  auth: AuthState;
-  // courses: CoursesState,
-  // lessons: LessonsState,
-}
+export interface AppState {}
 
-function authReducer(state: AuthState = initialAuthState, action): AuthState {
-  switch (action.type) {
-    case AuthActionTypes.LoginAction:
-      return {
-        loggedIn: true,
-        user: action.payload.user,
-      };
-    default:
-      return state;
-  }
-}
-
-export const reducers: ActionReducerMap<AppState> = {
-  auth: authReducer,
-};
+export const reducers: ActionReducerMap<AppState> = {};
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
   ? []
